3f87f8afabede8a7b14a8c3a90fdfef26c927117
pangilinan-patrick <97028950+pangilinan-patrick@users.noreply.github.com>
2023-06-01 23:14:32 +0800

    smaller tech icons

--- app/components/Experience.tsx
+++ app/components/Experience.tsx
@@ -115,7 +115,7 @@ function Experience() {
                 key={id}
                 className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}
               >
-                <Icon size={100} className={`w-20 mx-auto ${color}`} />
+                <Icon size={70} className={`w-20 mx-auto ${color}`} />
                 <p className={`mt-4`}>{title}</p>
               </div>
             );
