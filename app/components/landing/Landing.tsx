f6d93da248ba07d027e66b5a3952f017e367926b
pangilinan-patrick <97028950+pangilinan-patrick@users.noreply.github.com>
2023-06-10 20:49:11 +0800

    add animation to landing

--- app/components/landing/Landing.tsx
+++ app/components/landing/Landing.tsx
@@ -4,6 +4,7 @@ import HeroImage from "app/assets/heroImage.jpg";
 import Image from "next/image";
 import { FaAngleRight } from "react-icons/fa";
 import ScrollLink from "../ScrollLink";
+import { motion } from "framer-motion";
 
 function Landing() {
   return (
@@ -18,36 +19,65 @@ function Landing() {
         className={`max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row`}
       >
         <div className={`flex flex-col justify-center h-full`}>
-          <h2 className="text-4xl sm:text-7xl font-bold">
+          {/* First Section appear from top  */}
+          <motion.h2
+            initial={{ opacity: 0, y: -100 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.8 }}
+            className="text-4xl sm:text-7xl font-bold"
+          >
             I&apos;m a Front-End Developer
-          </h2>
-          <p className={`text-gray-400 py-4 sm:mr-28`}>
+          </motion.h2>
+
+          {/* Second Section appear from left  */}
+          <motion.p
+            initial={{ opacity: 0, x: -100 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.5, delay: 0.5 }}
+            className="text-gray-400 py-4 sm:mr-28"
+          >
             Hi, I&apos;m Patrick Pangilinan! I&apos;m a diligent 4th Year
             Computer Science Student with skills in programming, and
             specialization in Front-end Development. Committed to continuous
             learning and professional growth, I consistently seek out new
             opportunities to expand my skills and knowledge.
-          </p>
+          </motion.p>
 
-          <ScrollLink
-            href="#portfolio"
-            className={`group w-fit px-7 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500`}
-            aria-label="Go to portfolio section"
+          {/* Third Section appear from bottom  */}
+          <motion.div
+            initial={{ opacity: 0, y: 100 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, delay: 0.8 }}
           >
-            Portfolio
-            <span className={`group-hover:rotate-90 duration-300 ml-1`}>
-              <FaAngleRight />
-            </span>
-          </ScrollLink>
+            <ScrollLink
+              href="#portfolio"
+              className={`group w-fit px-7 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500`}
+              aria-label="Go to portfolio section"
+            >
+              Portfolio
+              <span className={`group-hover:rotate-90 duration-300 ml-1`}>
+                <FaAngleRight />
+              </span>
+            </ScrollLink>
+          </motion.div>
         </div>
 
+        {/* Last Section Appear from top */}
         <div>
-          <Image
-            src={HeroImage}
-            alt="portrait of patrick"
-            priority={true}
-            className={`rounded-2xl mx-auto w-2/4 md:w-full my-20`}
-          />
+          <motion.div
+            initial={{ opacity: 0, y: -100 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, delay: 1 }}
+          >
+            <div>
+              <Image
+                src={HeroImage}
+                alt="portrait of patrick"
+                priority={true}
+                className={`rounded-2xl mx-auto w-2/4 md:w-full my-20`}
+              />
+            </div>
+          </motion.div>
         </div>
       </div>
     </div>
