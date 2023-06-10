49570614975d3691f35ef8b065210d17a6c3de35
pangilinan-patrick <97028950+pangilinan-patrick@users.noreply.github.com>
2023-06-10 21:10:02 +0800

    add animations to about page

--- app/components/about/About.tsx
+++ app/components/about/About.tsx
@@ -1,4 +1,6 @@
+"use client";
 import React from "react";
+import { motion } from "framer-motion";
 
 function About() {
   return (
@@ -9,21 +11,41 @@ function About() {
       <div
         className={`max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full`}
       >
-        {/* Header Title */}
-        <div className={`py-8 sm:pb-8 sm:pt-0`}>
+        {/* Header Title First - Appear from top */}
+        <motion.div
+          initial={{ opacity: 0, y: 70 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 0.3 }}
+          viewport={{ once: true, amount: 0.8 }}
+          className={`py-8 sm:pb-8 sm:pt-0`}
+        >
           <p className={`text-4xl font-bold inline border-b-4 border-gray-500`}>
             About Me
           </p>
-        </div>
+        </motion.div>
 
-        <p className={`text-xl mt-10`}>
+        {/* Second appear from left */}
+        <motion.p
+          initial={{ opacity: 0, y: 70 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 0.3 }}
+          viewport={{ once: true, amount: 0.8 }}
+          className={`text-xl mt-10`}
+        >
           I have experience and expertise in Front-end Development, with a
           strong inclination towards Graphic Design. My proficiency lies in
           technologies like ReactJS, VueJS with Quasar Framework, and Axios.
           Furthermore, I am well-versed in graphic design tools such as GIMP and
           Inkscape.
-        </p>
-        <p className={`text-xl mt-10`}>
+        </motion.p>
+        {/* Third appear from left */}
+        <motion.p
+          initial={{ opacity: 0, y: 70 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 0.3 }}
+          viewport={{ once: true, amount: 0.8 }}
+          className={`text-xl mt-10`}
+        >
           Beyond technical skills, I possess excellent teamwork and leadership
           abilities, allowing me to effectively collaborate with others and lead
           projects towards successful outcomes. I am enthusiastic about
@@ -31,7 +53,7 @@ function About() {
           combine my technical expertise with my creative mindset. I am
           confident that my skills and passion will contribute to the success of
           any project I am involved in.
-        </p>
+        </motion.p>
       </div>
     </div>
   );
