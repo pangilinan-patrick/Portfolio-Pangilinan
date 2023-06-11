d2aced1a4f659d6645c8bc7e610349151a121607
pangilinan-patrick <97028950+pangilinan-patrick@users.noreply.github.com>
2023-06-11 17:17:27 +0800

    fix bug with submit button not appearing

--- app/components/contact/Contact.tsx
+++ app/components/contact/Contact.tsx
@@ -134,7 +134,7 @@ function Contact() {
               initial={{ opacity: 0, y: 70 }}
               whileInView={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.5, delay: 0.3 }}
-              viewport={{ once: true, amount: 0.8 }}
+              viewport={{ once: true }}
               className={`text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300`}
             >
               Let&apos;s talk
